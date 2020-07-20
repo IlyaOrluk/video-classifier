6c774c513de956f86a21c1fb4b57fcdc6902482a
IlyaOrluk <misfits120@mail.ru>
2020-07-20 19:57:03 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import * as ml5 from 'ml5'
 import p5 from 'p5'
 
 // Initialize the Image Classifier method with MobileNet. A callback needs to be passed.
-let classifier
+// let classifier
 
 // A variable to hold the image we want to classify
 let video
 let poseNet
-let pose
 
 let mobileNet
 
@@ -28,17 +27,11 @@ const init = (p) => {
       console.log(results)
       videoClassifierResults = results
       mobileNet.predict(gotResults)
-      
-
-
-      // createDiv(`Label: ${results[0].label}`)
-      // createDiv(`Confidence: ${nf(results[0].confidence, 0, 2)}`)
     }
   }
 
   p.preload = function() {
     console.log('preload')
-
   }
 
   p.setup = function(VIDEO) {
@@ -46,16 +39,9 @@ const init = (p) => {
     p.createCanvas(640, 600)
     video = p.createCapture(VIDEO)
     video.hide()
-    poseNet = ml5.poseNet(video, modelLoaded)
-    poseNet.on('pose', getPoses)
-
-    mobileNet = ml5.imageClassifier('MobileNet', video, modelReady)
+    mobileNet = ml5.imageClassifier('MobileNet', video, modelLoaded)
   }
 
-  function getPoses(poses) {
-    pose = poses[0] && poses[0].pose
-    // console.log(poses[0] && poses[0].pose)
-  }
 
   function modelLoaded() {
     console.log('poseNet ready!')
@@ -70,18 +56,6 @@ const init = (p) => {
     p.fill(255)
     p.textSize(24)
     p.text(videoClassifierResults && videoClassifierResults[0].label, 50, 520)
-    // p.text(videoClassifierResults && videoClassifierResults[1].label, 50, 100)
-    // p.text(videoClassifierResults && videoClassifierResults[2].label, 50, 150)
-    // if(pose) {
-    //   p.fill(255,0,0)
-    //   p.circle(pose.nose.x, pose.nose.y, 30)
-    //   p.fill(55,100,0)
-    //   p.circle(pose.leftEye.x, pose.leftEye.y, 25)
-    //   p.fill(55,100,0)
-    //   p.circle(pose.rightEye.x, pose.rightEye.y, 25)
-    //   p.fill(55,100,0)
-    //   p.circle(pose.rightShoulder.x, pose.rightShoulder.y, 25)
-    // } 
   }
 }
 
